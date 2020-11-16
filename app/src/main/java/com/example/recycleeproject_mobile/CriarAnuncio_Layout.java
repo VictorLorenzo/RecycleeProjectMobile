3af5918c24cc2255c64794a50dc16722cde678c4
victo <victor2003lorenzo@gmail.com>
2020-11-15 23:17:29 -0400

    Teste da Publicação

--- app/src/main/java/com/example/recycleeproject_mobile/CriarAnuncio_Layout.java
+++ app/src/main/java/com/example/recycleeproject_mobile/CriarAnuncio_Layout.java
@@ -2,21 +2,20 @@ package com.example.recycleeproject_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class CriarAnuncio_Layout extends AppCompatActivity {
 
     private Button pbtn;
     private Dialog mDialog;
+    public String titulo,ncontato,dproduto;
+    public EditText ctitulo,cncontato,cdproduto;
 
 
     @Override
@@ -26,18 +25,51 @@ public class CriarAnuncio_Layout extends AppCompatActivity {
 
         mDialog = new Dialog(this);
         pbtn = (Button) findViewById(R.id.Publicar_Button);
+        ctitulo = (EditText) findViewById(R.id.titulocampo);
+        cncontato = (EditText) findViewById(R.id.numerocampo);
+        cdproduto = (EditText) findViewById(R.id.descricaocampo);
+
+
         pbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 TextView cancelar;
+                Button sim;
+
                 mDialog.setContentView(R.layout.activity_layout_dialog);
                 cancelar = (TextView) mDialog.findViewById(R.id.CancelarPub);
+                sim = (Button) mDialog.findViewById(R.id.Publicar_sim);
+
                 cancelar.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         mDialog.dismiss();
                     }
                 });
+
+                sim.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        titulo = ctitulo.getText().toString().trim();
+                        ncontato = cncontato.getText().toString().trim();
+                        dproduto = cdproduto.getText().toString().trim();
+
+                        Intent i = new Intent(CriarAnuncio_Layout.this, anuncio_layout.class);
+                        Bundle parametros = new Bundle();
+
+                        parametros.putString("chave_titulo", titulo);
+                        parametros.putString("chave_contato", ncontato);
+                        parametros.putString("chave_dproduto", dproduto);
+
+                        i.putExtras(parametros);
+
+                        startActivity(i);
+
+                    }
+                });
+
+
                         mDialog.show();
 
             }
--- app/src/main/java/com/example/recycleeproject_mobile/anuncio_layout.java
+++ app/src/main/java/com/example/recycleeproject_mobile/anuncio_layout.java
@@ -2,13 +2,37 @@ package com.example.recycleeproject_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class anuncio_layout extends AppCompatActivity {
+
+    private TextView NQP,NP,D;
+    private  String nomeqp,nomep,desc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anuncio_layout);
+
+        NQP = findViewById(R.id.NomeQ_produto);
+        NP = findViewById(R.id.NomeProduto);
+        D = findViewById(R.id.Descricao);
+
+        Intent recebe = getIntent();
+
+        Bundle parametros = recebe.getExtras();
+
+        if(parametros != null){
+            nomeqp = parametros.getString("chave_titulo");
+            nomep = parametros.getString("chave_titulo");
+            desc = parametros.getString("chave_dproduto");
+        }
+
+        NQP.setText(nomep);
+        NP.setText(nomeqp);
+        D.setText(desc);
     }
+
 }
